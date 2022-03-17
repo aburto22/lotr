c3947dccc344eedead89550b614016bc525095d7
Alejandro A. Aburto Salazar <75444835+aburto22@users.noreply.github.com>
2022-03-17 18:15:11 +0100

    added exclusion of NaN in character card

--- client/src/components/CharacterCard/index.tsx
+++ client/src/components/CharacterCard/index.tsx
@@ -13,8 +13,8 @@ const CharacterCard = ({ name, id, gender, race }: CharacterCardProps) => {
   return (
     <div className="character-card">
       <h2 className="character-card__title">{name}</h2>
-      <p className="character-card__text">{gender}</p>
-      <p className="character-card__text">{race}</p>
+      {gender && gender !== 'NaN' && <p className="character-card__text">{gender}</p>}
+      {race && race !== 'NaN' && <p className="character-card__text">{race}</p>}
       <Link className="character-card__link" to={`/characters/${id}`}>more...</Link>
     </div>
   );
