b7477fae698ad98ff02f1898e063aae88936557a
Alejandro A. Aburto Salazar <75444835+aburto22@users.noreply.github.com>
2022-03-17 12:38:17 +0100

    character page completed

--- client/src/components/Character/index.tsx
+++ client/src/components/Character/index.tsx
@@ -20,7 +20,23 @@ const Character = () => {
 
   return (
     <main className="character-main">
-      <h2 className="character-main__title">Name</h2>
+      {character
+        ? (
+          <>
+            <h2 className="character-main__title">{character.name}</h2>
+            <p className="character-main__info">Race: {character.race}</p>
+            <p className="character-main__info">Gender: {character.gender}</p>
+            <p className="character-main__info">Birth: {character.birth}</p>
+            <p className="character-main__info">Spouse: {character.spouse}</p>
+            <p className="character-main__info">Death: {character.death}</p>
+            <p className="character-main__info">Realm: {character.realm}</p>
+            <p className="character-main__info">Hair: {character.hair}</p>
+            <p className="character-main__info">Height: {character.height}</p>
+            <a href={character.wikiUrl} className="character-main__link">Visit wikipedia</a>
+          </>
+        ) 
+        : 'loading'
+      }
     </main>
   );
 }
