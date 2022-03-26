bcff891651c9aa60d79f697ca263c7f8d48b2adb
Alejandro A. Aburto Salazar <75444835+aburto22@users.noreply.github.com>
2022-03-26 09:58:20 +0100

    added eslint to ts react airbnb project

---
+++ client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    project: `${__dirname}/tsconfig.json`
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint"
+  ],
+  rules: {
+  },
+  ignorePatterns: ["build/", "node_modules/", ".eslintrc.js"],
+}
